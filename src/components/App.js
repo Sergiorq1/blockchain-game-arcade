4c70a0997f7beb8cb4d4e8aabfade420bd10091a
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2021-10-15 13:38:51 -0700

    loaded all blockchain data onto website

--- src/components/App.js
+++ src/components/App.js
@@ -34,7 +34,22 @@ class App extends Component {
     const networkId = await web3.request({ method: 'net_version'})
     const networkData = MemoryToken.networks[networkId]
     if (networkData) {
+      const abi = MemoryToken.abi
       const address = networkData.address
+      const token = new web3.eth.Contract(abi, address)
+      this.setState({ token: token})
+      const totalSupply = await token.methods.totalSupply().call()
+      this.setState({ totalSupply: totalSupply })
+
+      //Load tokens
+      let balanceOf = await token.methods.balanceOf(accounts[0]).call()
+      for (let i=0; i<balanceOf; i++) {
+        let id = await token.methods.tokenOfOwnerByIndex(accounts[0], i).call()
+        let tokenURI = await token.methods.tokenURI(id).call()
+        this.setState({
+          tokenURIs: [...this.state.tokenURIs, tokenURI]
+        })
+      } 
     } else {
       alert('Smart contract not deployed to detected network')
     }
@@ -43,7 +58,9 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      account: '0x0'
+      account: '0x0',
+      token: null,
+      tokenURIs: [],
     }
   }
 
